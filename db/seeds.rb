7befdb072725028d5415809467bc852ab97a69bc
keisuke-333 <keikubo.999@gmail.com>
2021-03-27 14:20:12 +0900

    todosテーブル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+SAMPLE_TODOS = [
+  {
+    name: "test1",
+  },
+  {
+    name: "test2",
+  },
+  {
+    name: "test3",
+  },
+]
+
+SAMPLE_TODOS.each do |todo|
+  Todo.create(todo)
+end
